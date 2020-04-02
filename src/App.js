945bdf51a1d7106312606de8f07a98d4277503c5
Yazan Tarifi <yazantarifi98@gmail.com>
2020-04-02 04:20:08 +0100

    add footer

--- src/App.js
+++ src/App.js
@@ -1,12 +1,33 @@
 import React from 'react';
 import './App.css';
 import ToolbarComponent from './components/ToolbarComponent';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import FooterComponent from './components/FooterComponent';
 
 function App() {
   return (
-    <div className="AppContainer">
-      <ToolbarComponent />
+    <Router>
+      <div className="AppContainer">
+        <ToolbarComponent />
+        <Switch>
+          <Route path="/home">
+
+          </Route>
+          <Route path="/projects">
+            
+          </Route>
+          <Route path="/about">
+            
+          </Route>
+        </Switch>
+        <FooterComponent />
     </div>
+    </Router>
   );
 }
 
