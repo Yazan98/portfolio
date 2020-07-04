44d02b9f461e75752326966ebd97b3748c947128
Yazan Tarifi <ytarifi@noventapp.com>
2020-07-04 01:26:27 +0100

    Updates

---
+++ precache-manifest.3ea61f2b2f3f6df8616a76e6f78dbe5c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6dd8b5f89b02bea119713c5ba41265bf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "25c94c9f7b124f666e7e",
+    "url": "/static/css/main.f1039cf2.chunk.css"
+  },
+  {
+    "revision": "a377d715c92232001a98",
+    "url": "/static/js/2.3dbdf456.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.3dbdf456.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "25c94c9f7b124f666e7e",
+    "url": "/static/js/main.c5dde7dc.chunk.js"
+  },
+  {
+    "revision": "4a0d143f42e781affabb",
+    "url": "/static/js/runtime-main.c2e95d27.js"
+  }
+]);
