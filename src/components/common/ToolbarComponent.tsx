2b3eef91f0670816e9ca575a8921f58c5feb38a0
DESKTOP-17F9CUB\yazan <yazantarifi98@gmail.com>
2020-09-04 10:00:36 -0700

    Fixes

--- src/components/common/ToolbarComponent.tsx
+++ src/components/common/ToolbarComponent.tsx
@@ -8,10 +8,10 @@ export default function ToolbarComponent() {
     return (
         <header className="ToolbarComponent">
             <ul style={{ backgroundColor: InfoManager.WHITE_MODE_BACKGROUND_COLOR }}>
-                <li><NavLink to="/"><a className="active" href="#home" style={{ color: InfoManager.BLUE_LINE_HEIGHT, marginLeft: "50px" }}>{ infoManager.getPersonalInfo().name }</a></NavLink></li>
-                <li  style={{float: "right", marginRight: "50px"}}><NavLink to="/services"><a href="#">Services</a></NavLink></li>
-                <li style={{float: "right"}}><NavLink to="/about"><a href="#">About</a></NavLink></li>
-                <li  style={{float: "right"}}><NavLink to="/projects"><a href="#">Projects</a></NavLink></li>
+                <li><NavLink to="/"><a className="active" style={{ color: InfoManager.BLUE_LINE_HEIGHT, marginLeft: "50px" }}>{ infoManager.getPersonalInfo().name }</a></NavLink></li>
+                <li  style={{float: "right", marginRight: "50px"}}><NavLink to="/services"><a>Services</a></NavLink></li>
+                <li style={{float: "right"}}><NavLink to="/about"><a>About</a></NavLink></li>
+                <li  style={{float: "right"}}><NavLink to="/projects"><a>Projects</a></NavLink></li>
             </ul>
         </header>
     );
