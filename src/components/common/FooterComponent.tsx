1db14f210b284c1fe92d76cb839176c53a904a93
DESKTOP-6JRHMBT\yazan <yazantarifi98@gmail.com>
2020-09-19 11:04:11 -0700

    Fixes

--- src/components/common/FooterComponent.tsx
+++ src/components/common/FooterComponent.tsx
@@ -8,8 +8,8 @@ import GithubLogo from "../images/min_github.png";
 export function FooterComponent() {
     return (
         <footer className="FooterComponent">
-            <div className="row">
-                <div className="col" id="Section">
+            <div className="FooterContent">
+                <div id="Section">
                     <h4>About My Projects</h4>
                     <p>Most of the projects that I work on are made to improve and develop from my own applications that facilitate the programming process and allow freedom to make very large applications that are developed very very quickly because of the application infrastructure that makes you reach your goal quickly faster than you imagined</p>
                     <h6>Follow Me On</h6>
@@ -22,8 +22,8 @@ export function FooterComponent() {
                     </div>
                     <p style={{ marginTop: "10px" }}>Yazan Tarifi @ 2020 All Rights Reserved</p>
                 </div>
-                <div className="col">
-                    <div className="Area" style={{textAlign: "center"}}>
+                <div id="Section" className={"ContactMeContainer"}>
+                    <div className="Area">
                         <TextField className="TextArea" id="outlined-basic" label="Please Enter Your Email" variant="outlined" inputMode="email"/>  <br/>
                         <TextField className="TextArea" id="outlined-basic" label="Please Enter Your Message" variant="outlined" />  <br/>
                         <a href="" className="SubmitButton">Submit</a>
