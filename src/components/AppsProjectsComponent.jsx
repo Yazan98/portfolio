844de0ba7900cc4de63f28f6b71305c399fa22d6
DESKTOP-6JRHMBT\yazan <yazantarifi98@gmail.com>
2020-10-28 23:46:13 -0700

    Add Grid Layout to Apps

--- src/components/AppsProjectsComponent.jsx
+++ src/components/AppsProjectsComponent.jsx
@@ -11,59 +11,74 @@ class AppsProjectsComponent extends Component {
         return (
             <div className={"Container"}>
 
-                <div className={"Item"}>
-                    <div className={"App"}>
-                        <img alt={"Wintrop"} src={Wintrop} />
-                    </div>
-                    <div className={"Card blue"}>
-                        <p className={"Type"}>Open Source</p>
-                        <h4>Wintrop</h4>
-                        <p>Weather Application Powered By Vortex , Latest Android Technologies With Managed Structure To Show Weather Status For Amman, Irbid, Aqaba Powered By Retrofit , Kotlin , MVVM</p>
-                       <div className={"Link"}>
-                           <NavLink className={"text"} to={"/project/wintrop"}>App Link</NavLink>
-                       </div>
-                    </div>
+                <div className={"Title"}>
+                    <h4>Mobile Applications Section</h4>
+                    <p>Here are some of the apps I've worked on</p>
                 </div>
 
-                <div className={"Item"}>
-                    <div className={"Card blue2 disable-margin"}>
-                        <p className={"Type"}>Open Source</p>
-                        <h4>Autohub</h4>
-                        <p>Github Client Powered By Vortex-io , Kotlin Language Based on Github Api V3 To Show Github User Info, The Porpouse of this project is to test Vortex Structure and Implementation</p>
-                        <div className={"Link"}>
-                            <NavLink className={"text"} to={"/project/autohub"}>App Link</NavLink>
-                        </div>
-                    </div>
-                    <div className={"App"}>
-                        <img alt={"Github"} src={Github} />
-                    </div>
-                </div>
+                <div className={"GridItemContainer"}>
+                    <div className={"GridContainer"}>
 
-                <div className={"Item"}>
-                    <div className={"App"}>
-                        <img alt={"Destiny"} src={Destiny} />
-                    </div>
-                    <div className={"Card green"}>
-                        <p className={"Type"}>Open Source</p>
-                        <h4>Destiny</h4>
-                        <p>This Project is a Training Project Powered By Vortex-io and Realm Database , Kotlin Language With Standalone Api Built in Spring Boot and Kotlin with MySql Database, Firebase Push Notification</p>
-                        <div className={"Link"}>
-                            <NavLink className={"text"} to={"/project/destiny"}>App Link</NavLink>
+                        <div className={"ItemContainer"}>
+                            <div className={"PrimaryItem"}>
+                                <img src={Github}/>
+                            </div>
+                            <div className={"HoverItems"}>
+                                <div className={"ItemsContainer"}>
+                                    <div className={"Components"}>
+                                        <p>Android Application</p>
+                                        <h4>Autohub</h4>
+                                        <p>An Github Client Built To Test Vortex Infrastructure For Android Applications With A Lot Of Features To Manage Github Accounts Personal Accounts, Repositories, Search Repository, Popular Repos, Orgs, Gists, Github Auth, With Kotlin Programming Language</p>
+                                    </div>
+                                </div>
+                            </div>
                         </div>
-                    </div>
-                </div>
 
-                <div className={"Item"}>
-                    <div className={"Card prple disable-margin"}>
-                        <p className={"Type"}>Closed Source</p>
-                        <h4>Drasati</h4>
-                        <p>An organizing platform for school students that aims to gather students and link them with audible lessons by providing a tree plan with the possibility to speak with teachers if a difference occurs on a certain point with the support feature of all classes</p>
-                        <div className={"Link"}>
-                            <NavLink className={"text"} to={"/project/drasati"}>App Link</NavLink>
+                        <div className={"ItemContainer"}>
+                            <div className={"PrimaryItem"}>
+                                <img src={Destiny}/>
+                            </div>
+                            <div className={"HoverItems"}>
+                                <div className={"ItemsContainer"}>
+                                    <div className={"Components"}>
+                                        <p>Android Application / Spring Boot Api</p>
+                                        <h4>Destiny</h4>
+                                        <p>Restaurant Application Powered By Vortex Infrastructure With Latest Technologies, Realm Database, Restful Api, Trainig Project and Testing For Vortex Testing</p>
+                                    </div>
+                                </div>
+                            </div>
                         </div>
-                    </div>
-                    <div className={"App"}>
-                        <img alt={"Drasati"} src={Drasati} />
+
+                        <div className={"ItemContainer"}>
+                            <div className={"PrimaryItem"}>
+                                <img src={Drasati}/>
+                            </div>
+                            <div className={"HoverItems"}>
+                                <div className={"ItemsContainer"}>
+                                    <div className={"Components"}>
+                                        <p>Android Application</p>
+                                        <h4>Drasati</h4>
+                                        <p>An organizing platform for school students that aims to gather students and link them with audible lessons by providing a tree plan with the possibility to speak with teachers if a difference occurs on a certain point with the support feature of all classes</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div className={"ItemContainer"}>
+                            <div className={"PrimaryItem"}>
+                                <img src={Wintrop}/>
+                            </div>
+                            <div className={"HoverItems"}>
+                                <div className={"ItemsContainer"}>
+                                    <div className={"Components"}>
+                                        <p>Android Application</p>
+                                        <h4>Wintrop</h4>
+                                        <p>Weather Application Powered By Vortex , Latest Android Technologies With Managed Structure To Show Weather Status For Amman, Irbid, Aqaba</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
                     </div>
                 </div>
 
