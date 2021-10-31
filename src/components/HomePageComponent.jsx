8bd345fa00d804d7ad45efb9e1fe8bc41c0a24c5
Yazan Tarifi <yazantarifi98@gmail.com>
2021-10-31 09:03:28 -0700

    Create Files Components Structure

--- src/components/HomePageComponent.jsx
+++ src/components/HomePageComponent.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import ScreenContainerComponent from './common/ScreenContainerComponent';
+import { HOME_PAGE_TITLE } from '../info/StringsUtils';
 
 export default function HomePageComponent() {
   return (
-    <ScreenContainerComponent className="HomePageContainer" title="Yazan Tarifi Portfolio - Home Page">
+    <ScreenContainerComponent className="HomePageContainer" title={HOME_PAGE_TITLE}>
       <h1>Title</h1>
     </ScreenContainerComponent>
   );
--- src/components/NotFoundPageViewComponent.jsx
+++ src/components/NotFoundPageViewComponent.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import ScreenContainerComponent from './common/ScreenContainerComponent';
+import { NOT_FOUND_TITLE } from '../info/StringsUtils';
 
 export default function NotFoundPageViewComponent() {
   return (
-    <ScreenContainerComponent title="Yazan Tarifi - Page Not Found (404)" className="NotFoundPageContainer">
+    <ScreenContainerComponent title={NOT_FOUND_TITLE} className="NotFoundPageContainer">
       <h1>Not Found</h1>
     </ScreenContainerComponent>
   );
