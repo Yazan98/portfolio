268e5cfd55fc145b43cd7000720eb55bf8cce99e
Yazan Tarifi <yazantarifi98@gmail.com>
2022-01-08 21:34:46 +0200

    Add Tailwind Css, Revamp Nav Bar to be Responsive

--- src/components/common/InnerToolbarComponent.jsx
+++ src/components/common/InnerToolbarComponent.jsx
@@ -8,32 +8,21 @@ import { PRIMARY_COLOR } from '../../info/ColorUtils';
 
 export default function InnerToolbarComponent({ isTitleWhite }) {
   return (
-    <nav>
-      <Box sx={{ flexGrow: 1 }}>
-        <Toolbar>
-          <Grid
-            justify="space-between"
-            container
-            spacing={24}
-          >
-            <Grid item>
-              <Typography variant="h6" style={{ color: isTitleWhite ? 'white' : PRIMARY_COLOR }} component="div" sx={{ flexGrow: 1 }}>
-                Yazan Tarifi
-              </Typography>
-            </Grid>
-            <Grid item>
-              <div>
-                <NavLink style={{ marginLeft: '1em', color: 'white' }} to="/">Home</NavLink>
-                <NavLink style={{ marginLeft: '1em', color: 'white' }} to="/projects">Projects</NavLink>
-                <NavLink style={{ marginLeft: '1em', color: 'white' }} to="/skills">Skills</NavLink>
-                <NavLink style={{ marginLeft: '1em', color: 'white' }} to="/about">About</NavLink>
-                <NavLink style={{ marginLeft: '1em', color: 'white' }} to="/archive">Archive</NavLink>
-                <NavLink style={{ marginLeft: '1em', color: 'white' }} to="/resume">Resume</NavLink>
-              </div>
-            </Grid>
-          </Grid>
-        </Toolbar>
-      </Box>
-    </nav>
+      <nav className={"flex justify-between items-center h-16 text-white relative font-mono"} role={"navigation"}>
+        <NavLink style={{ fontSize: "large", color: isTitleWhite ? "white": PRIMARY_COLOR }} to={"/"} className={"pl-8"}>Yazan Tarifi</NavLink>
+        <div className={"px-4 cursor-pointer md:hidden"}>
+          <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4 6h16M4 12h16M4 18h16" />
+          </svg>
+        </div>
+        <div className={"pr-4 md:block hidden"}>
+          <NavLink style={{ color: isTitleWhite ? "white": location.pathname === "/" ? PRIMARY_COLOR : "" }} className={"p-2"} to="/">Home</NavLink>
+          <NavLink style={{ color: isTitleWhite ? "white": location.pathname === "/projects" ? PRIMARY_COLOR : ""}} className={"p-2"} to="/projects">Projects</NavLink>
+          <NavLink style={{ color: isTitleWhite ? "white": location.pathname === "/skills" ? PRIMARY_COLOR : "" }} className={"p-2"} to="/skills">Skills</NavLink>
+          <NavLink style={{ color: isTitleWhite ? "white": location.pathname === "/about" ? PRIMARY_COLOR : "" }} className={"p-2"} to="/about">About</NavLink>
+          <NavLink style={{ color: isTitleWhite ? "white": location.pathname === "/archive" ? PRIMARY_COLOR : "" }} className={"p-2"} to="/archive">Archive</NavLink>
+          <NavLink style={{ color: isTitleWhite ? "white": location.pathname === "/resume" ? PRIMARY_COLOR : "" }} className={"p-2"} to="/resume">Resume</NavLink>
+        </div>
+      </nav>
   );
 }
--- src/components/common/ToolbarComponent.jsx
+++ src/components/common/ToolbarComponent.jsx
@@ -1,43 +1,26 @@
 import React from 'react';
-import Toolbar from '@material-ui/core/Toolbar';
-import Box from '@material-ui/core/Box';
-import Typography from '@material-ui/core/Typography';
 import { NavLink } from 'react-router-dom';
-import { Grid } from '@material-ui/core';
 import { PRIMARY_COLOR } from '../../info/ColorUtils';
 import { useLocation } from 'react-router-dom'
 
 export default function ToolbarComponent() {
-  // const isSmall = useMediaQuery(theme.breakpoints.down('sm'));
-  // Use This when Do Mobile Design
   const location = useLocation();
   return (
-    <nav>
-      <Box sx={{ flexGrow: 1 }}>
-        <Toolbar>
-          <Grid
-            justify="space-between"
-            container
-            spacing={24}
-          >
-            <Grid item>
-              <Typography variant="h6" style={{ color: PRIMARY_COLOR }} component="div" sx={{ flexGrow: 1 }}>
-                <NavLink to={"/"}>Yazan Tarifi</NavLink>
-              </Typography>
-            </Grid>
-            <Grid item>
-              <div>
-                <NavLink style={{ marginLeft: '1em', color: location.pathname === "/" ? PRIMARY_COLOR : "" }} to="/">Home</NavLink>
-                <NavLink style={{ marginLeft: '1em', color: location.pathname === "/projects" ? PRIMARY_COLOR : ""}} to="/projects">Projects</NavLink>
-                <NavLink style={{ marginLeft: '1em', color: location.pathname === "/skills" ? PRIMARY_COLOR : "" }} to="/skills">Skills</NavLink>
-                <NavLink style={{ marginLeft: '1em', color: location.pathname === "/about" ? PRIMARY_COLOR : "" }} to="/about">About</NavLink>
-                <NavLink style={{ marginLeft: '1em', color: location.pathname === "/archive" ? PRIMARY_COLOR : "" }} to="/archive">Archive</NavLink>
-                <NavLink style={{ marginLeft: '1em', color: location.pathname === "/resume" ? PRIMARY_COLOR : "" }} to="/resume">Resume</NavLink>
-              </div>
-            </Grid>
-          </Grid>
-        </Toolbar>
-      </Box>
+    <nav className={"flex justify-between items-center h-16 bg-white text-black relative shadow-md font-mono"} role={"navigation"}>
+      <NavLink style={{ fontSize: "large", color: PRIMARY_COLOR }} to={"/"} className={"pl-8"}>Yazan Tarifi</NavLink>
+        <div className={"px-4 cursor-pointer md:hidden"}>
+            <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4 6h16M4 12h16M4 18h16" />
+            </svg>
+        </div>
+      <div className={"pr-4 md:block hidden"}>
+        <NavLink style={{ color: location.pathname === "/" ? PRIMARY_COLOR : "" }} className={"p-2"} to="/">Home</NavLink>
+        <NavLink style={{ color: location.pathname === "/projects" ? PRIMARY_COLOR : ""}} className={"p-2"} to="/projects">Projects</NavLink>
+        <NavLink style={{ color: location.pathname === "/skills" ? PRIMARY_COLOR : "" }} className={"p-2"} to="/skills">Skills</NavLink>
+        <NavLink style={{ color: location.pathname === "/about" ? PRIMARY_COLOR : "" }} className={"p-2"} to="/about">About</NavLink>
+        <NavLink style={{ color: location.pathname === "/archive" ? PRIMARY_COLOR : "" }} className={"p-2"} to="/archive">Archive</NavLink>
+        <NavLink style={{ color: location.pathname === "/resume" ? PRIMARY_COLOR : "" }} className={"p-2"} to="/resume">Resume</NavLink>
+      </div>
     </nav>
   );
 }
